6ac30f2ff4311aa4cd8dbdf4173e00d9926999a9
Colin Hill <colin@hill.fm>
2026-07-28 14:22:56 -0600

    Reflect uncached MSVC plugin reloads

--- tests/app/application_test.cpp
+++ tests/app/application_test.cpp
@@ -2268,7 +2268,11 @@ end architecture rtl;
 #else
   assert(second->cache_hit);
 #endif
+#if defined(_WIN32)
+  assert(first->systemc_hierarchy != second->systemc_hierarchy);
+#else
   assert(first->systemc_hierarchy == second->systemc_hierarchy);
+#endif
   const auto child_q = first->design.find_signal("tb.u_child.value");
   assert(child_q);
   const auto paths = first->design.signal_paths();
